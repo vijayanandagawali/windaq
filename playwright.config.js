558a8168528789a488f3efc77521d8a6c29f8322
vijayanandagawali <vijayanandagawali@gmail.com>
2026-09-20 18:15:42 +0530

    chore: remove dead legacy root HTML/JS files and lock E2E test matrix

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
+  testMatch: 'autonomous_qa_matrix.spec.js',
   timeout: 60000,
   use: {
     baseURL: 'http://localhost:3000',
